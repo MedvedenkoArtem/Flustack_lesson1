528e6e96870cd141cd72293125666954b2e5680a
MedvedenkoArtem <artem.medvedenko1@gmail.com>
2026-02-08 15:26:47 +0100

    Homework 03: Login form with reusable components

--- lesson_01/src/App.jsx
+++ lesson_01/src/App.jsx
@@ -1,16 +1,21 @@
 // Lessons
 // import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
+// import Lesson_03 from "./lessons/Lesson_03/Lesson_03";
 
 // Homeworks
-import Homework_02 from "./homeworks/Homework_02/Homework_02";
+// import Homework_02 from "./homeworks/Homework_02/Homework_02";
+// import Homework_02 from "./homeworks/Homework_02/Homework_02";
+import Homework_03 from "./homeworks/Homework_03/Homework_03";
 
 function App() {
   return (
     <>
       {/* LESSONS */}
       {/* <Lesson_02 /> */}
+      {/* <Lesson_03 /> */}
       {/* HOMEWORKS */}
-      <Homework_02 />
+      {/* <Homework_02 /> */}
+      <Homework_03 />
     </>
   );
 }
--- lesson_01/src/lessons/componemts/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import "./styles.css";
-
-function Button() {
-  return <button className="button_component">Send</button>;
-}
-
-export default Button;
